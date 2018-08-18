8a25155676fe4a22f64700dc04a172db582159e9
Vincent Karuri <vincentmkaruri@gmail.com>
2018-08-18 22:49:48 +0300

    Code refactor

--- RemoveDuplicates.js
+++ RemoveDuplicates.js
@@ -1,40 +1,54 @@
 function removeDuplicates(str) {
-    var set = new Array();
+    var map = new Array();
     var duplicates = new Array();
     var duplicatesStr = "";
+
+    // O(N) run time in length of string
+    var result = deDuplicateStr(str, map, duplicates);
+    console.log("String with duplicates removed is: " + result + "\n");
+
+    duplicatesStr = printDuplicateCharsAndFreqs(str, duplicates, duplicatesStr, map);
+    printStrsBackWardAndForward(duplicatesStr, result);
+}
+
+function deDuplicateStr(str, map, duplicates) {
     var result = "";
     // O(N) run time in length of string
     for (var i = 0; i < str.length; i++) {
         var char = str[i];
-        if (!(char in set)) {
+        if (!(char in map)) {
             result += str[i];
-            set[char] = 0;
+            map[char] = 0;
         } else {
             duplicates[char] = 1;
-            set[char] = set[char] + 1;
+            map[char] = map[char] + 1;
         }
     }
-    console.log("String with duplicates removed is: " + result + "\n");
+    return result;
+}
 
+function printDuplicateCharsAndFreqs(str, duplicates, duplicatesStr, map) {
     console.log("Duplicate characters and their frequencies: ");
     for (var i = 0; i < str.length; i++) {
         var char = str[i];
         if (char in duplicates) {
-            console.log(char + " => " + set[char]);
+            console.log(char + " => " + map[char]);
             duplicatesStr += char;
             delete duplicates[char];
         }
     } 
+    return duplicatesStr;
+}
 
-    // sorting will take O(NlogN) which ends up being worst time complexity
+function printStrsBackWardAndForward(duplicatesStr, result) {
+    function backwardCompare(char1, char2) {
+     return char2.localeCompare(char1);
+    }
+     // sorting will take O(NlogN) which ends up being worst time complexity
     console.log("\nDuplicate string sorted forward: " + duplicatesStr.split("").sort().join("") + "\n");
     console.log("Duplicate string sorted backward: " + duplicatesStr.split("").sort(backwardCompare).join("") + "\n");
     console.log("\nDe-duplicated string sorted forward: " + result.split("").sort().join("") + "\n");
-    console.log("De-duplicate string sorted backward: " + result.split("").sort(backwardCompare).join("") + "\n");
-}
-
-function backwardCompare(char1, char2) {
-    return char2.localeCompare(char1);
+    console.log("De-duplicated string sorted backward: " + result.split("").sort(backwardCompare).join("") + "\n");
 }
 
 removeDuplicates("testertester");
